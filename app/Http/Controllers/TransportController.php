5c7ac7ce090c7672646d375945a3c382bb2a25ea
GowellKent <Gowellkent1@gmail.com>
2023-08-10 13:59:28 +0700

    added API CRUD function to transport controller

--- app/Http/Controllers/TransportController.php
+++ app/Http/Controllers/TransportController.php
@@ -2,10 +2,173 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\tvl_transport;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class TransportController extends Controller
 {
     //
-    
+    function getAll(){
+        return tvl_transport::all();
+    }
+
+    function find(Request $request){
+        
+        $validate = Validator::make($request->all(), [
+            'tt_kode' => 'required'
+        ]);
+        
+        //response error validation
+        if ($validate->fails()) {
+            return response()->json($validate->errors(), 400);
+        }
+        
+        $tt_kode = $request->input('tt_kode');
+
+        $resp = tvl_transport::find($tt_kode);
+        $resp['foto'] = tvl_transport::find($tt_kode)->fotos;
+
+        if(count($resp->all()) > 0){
+            return $resp;
+        }
+        else{
+            return response()->json([
+                'success' => false,
+                'message' => 'Transport Data Not Found',
+            ], 404);
+        }
+    }
+
+    function store(Request $request){
+        $validate = Validator::make($request->all(), [
+            'tt_nama' => 'required',
+            'tt_provinsi_asal' => 'required',
+            'tt_kota_asal' => 'required',
+            'tt_provinsi_tujuan' => 'required',
+            'tt_kota_tujuan' => 'required',
+            'tt_pax' => 'required',
+            'tt_harga' => 'required'
+        ]);
+
+        //response error validation
+        if ($validate->fails()) {
+            return response()->json($validate->errors(), 400);
+        }
+
+        // $data = array_keys((array)$request->input());
+        $resp = DB::table("tvl_transports")->max('tt_kode') + 1 ;
+
+        $data = array();
+        foreach (array_keys((array)$request->input()) as $value) {
+            $data[$value] = $request->input($value);
+            $data['tt_kode'] = $resp;
+        }
+
+
+        //save to DB
+        $bus = tvl_transport::create($data);
+
+        if ($bus) {
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Data Bus Created',
+                'data'    => $data
+            ], 201);
+        }
+
+        //failed save to database
+        return response()->json([
+            'success' => false,
+            'message' => 'Objek Tujuan Failed to Save',
+        ], 409);
+    }
+
+    public function update(Request $request, tvl_transport $transport){
+        $tt_kode = $request->input('tt_kode');
+
+        $validate = Validator::make($request->all(), [
+            'tt_kode' => 'required'
+        ]);
+
+        // response error validation
+        if ($validate->fails()) {
+            return response()->json($validate->errors(), 400);
+        }
+
+        //find objek tujuan by tb_kode
+        $transport = tvl_transport::where('tt_kode', $tt_kode)->firstOrFail();
+
+        //save to DB
+
+        if ($transport) {
+
+            $data = array();
+            foreach (array_keys((array)$request->input()) as $value) {
+                $data[$value] = $request->input($value);
+            }
+
+            $transport = DB::table("tvl_transports")
+                ->where('tt_kode', $tt_kode)
+                ->limit(1)
+                ->update($data);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Transport Data Updated',
+                'data'    => $request->input()
+            ], 201);
+        }
+
+        //failed save to database
+        return response()->json([
+            'success' => false,
+            'message' => 'Transport Data Failed to Update',
+            'data' => $request->input()
+        ], 409);
+    }
+
+    public function delete(Request $request, tvl_transport $transport){
+        $tt_kode = $request->input('tt_kode');
+
+        $validate = Validator::make($request->all(), [
+            'tt_kode' => 'required'
+        ]);
+
+        // response error validation
+        if ($validate->fails()) {
+            return response()->json($validate->errors(), 400);
+        }
+
+        //find objek tujuan by tt_kode
+        $transport = tvl_transport::where('tt_kode', $tt_kode)->firstOrFail();
+
+        //save to DB
+
+        if ($transport) {
+
+            $transport =    DB::table("tvl_transports")
+                ->where('tt_kode', $tt_kode)
+                ->limit(1)
+                ->delete();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Transport Data Deleted',
+                'data'    => $transport
+            ], 201);
+        } else {
+            //failed save to database
+            return response()->json([
+                'success' => false,
+                'message' => 'Transport Data Failed to delete',
+            ], 409);
+        }
+    }
+
+    public function searchByRoute(Request $request){
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\FotoTransportController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProvKotaController;
+use App\Http\Controllers\TransportController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -29,6 +30,14 @@ Route::group(['prefix' => 'daerah', 'middleware' => 'auth:sanctum'], function(){
 Route::group(['prefix' => 'admin', 'middleware' => ['auth:sanctum', 'isAdmin']], function(){
     Route::post('/delFoto', [FotoTransportController::class, 'delFoto']);
     Route::get('/findFoto', [FotoTransportController::class, 'findFoto']);
+});
+
+Route::group(['prefix' => 'transport'], function(){
+    Route::get('/all', [TransportController::class, 'getAll']);
+    Route::get('/find', [TransportController::class, 'find']);
+    Route::post('/store', [TransportController::class, 'store']);
+    Route::post('/update', [TransportController::class, 'update']);
+    Route::post('/delete', [TransportController::class, 'delete']);
 });
 
 Route::post('/register', [LoginController::class, 'register']);
